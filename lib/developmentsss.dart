bc716b48d201134c24a26419022b0504a7a036e7
mahmoudsalem1907 <62968091+mahmoudsalem1907@users.noreply.github.com>
2022-02-24 01:43:12 +0200

    developmentsadd

---
+++ lib/developmentsss.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class developmentsss extends StatelessWidget {
+  const developmentsss({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('data'),
+      ),
+    );
+  }
+}
